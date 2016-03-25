1116ca5fbed9e1609b9d8fd1cccbebbc717207aa
Wenson Tsai <wensontsai@gmail.com>
2016-03-25 17:36:10 -0400

    fack

--- server/app/routes/answerRoutes.js
+++ server/app/routes/answerRoutes.js
@@ -1,12 +1,11 @@
 exports.submitAnswer = function(Answer){
   return function(req, res, next){
     console.log(req.body);
-    // Answer.findOne({ emailCode: req.body.emailCode, promptId: req.body.promptId }, function(err, answer){
-      Answer.findOne({ }, function(err, answer){
+    Answer.findOne({ emailCode: req.body.emailCode, promptId: req.body.promptId }, function(err, answer){
       if(err) return console.error(err);
-      if (answer) {
-        return res.json( {success: false, message: 'This question has already been answered for this exam session!'} );
-      } else {
+      // if (answer) {
+      //   return res.json( {success: false, message: 'This question has already been answered for this exam session!'} );
+      // } else {
         var now = new Date();
 
         var answer = new Answer({
@@ -21,7 +20,7 @@ exports.submitAnswer = function(Answer){
           if(err) return console.error(err);
           res.json(answer);
         });
-      }
+      // }
     });
   };
 };
