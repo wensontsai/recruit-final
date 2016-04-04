f59d6d2ba299b81ee3347086e86f95e7e93a435d
Wenson Tsai <wensontsai@gmail.com>
2016-04-04 17:21:51 -0400

    testing env files

--- server/config.sample.js
+++ server/config.sample.js
@@ -1,3 +1,6 @@
+// CREATE TWO FILES:  'config.js' <~ MAIN CONFIG FILE
+//                    'config_test.js' <~ TEST CONFIG FILE
+
 module.exports = {
     'secret': '<secret>',
     'database': 'mongodb://localhost/<database_name>'
