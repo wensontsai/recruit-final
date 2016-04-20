2d1fbaa53cd56e4a83d2043d36adc7e2ed3dc455
Wenson Tsai <wensontsai@gmail.com>
2016-04-20 11:06:53 -0400

    cleanup

--- server/build/models/prompt.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-module.exports = mongoose.model('Prompt', new Schema({
-  question: { type: String, required: true }
-}));
-//# sourceMappingURL=prompt.js.map
